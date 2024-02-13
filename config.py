9c71db73a5fdedfee8d3a3d44d86e415ec8c7dba
lingjinzhou <lingjineri@outlook.com>
2024-02-13 19:59:07 +0800

    blah blah

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+import data_util
 import datetime
 
+ss_util = data_util.StockSummaryWrapper()
 
 class Settings:
     def __init__(self):
         self.start_date = datetime.date.today()
         self.end_date = self.start_date
+        self.rolling_size = 130 #half year
+        self.rtn_horizon = 1 #daily 1, weekly 5...
 
 
 ticker_1 = '000001'
 ticker_2 = '000002'
+
---
+++ utils.py
@@ -0,0 +1,3 @@
+
+def get
+    data_util.simple_price_data(ticker, frequency)
